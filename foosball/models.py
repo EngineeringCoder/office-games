f77f01217e300f8ed0ab6e5f433731d4485a7b23
Kjetil Lorentzen <kjeloren@gmail.com>
2019-12-10 20:47:31 +0100

    started work on models + some testing

--- foosball/models.py
+++ foosball/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Kamp(models.Model):
+    tidspunkt = models.DateTimeField(auto_now_add=True)
+    vinner1 = models.ForeignKey(Spiller,on_delete=models.CASCADE,null=False)
+    vinner2 = models.ForeignKey(Spiller,on_delete=models.CASCADE)
+    taper1 = models.ForeignKey(Spiller,on_delete=models.CASCADE)
+    taper2 = models.ForeignKey(Spiller,on_delete=models.CASCADE)
+
+
+    __str__(self):
+        return self.id
+    
+    # get_absolute_url(self):
+        # return reverse("_detail", kwargs={"pk": self.pk})
+    
--- foosball/tests.py
+++ foosball/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class HomepageTest(SimpleTestCase):
+
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('foosball:home'))
+        self.assertEqual(response.status_code,200)
