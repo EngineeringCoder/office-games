c78ead4470f227e768dfa4c50b369273e63da101
Kjetil Lorentzen <kjeloren@gmail.com>
2020-01-23 20:25:43 +0100

    added CreateView for profile app

--- profile/urls.py
+++ profile/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
-from .views import ProfileListView
+from .views import ProfileListView,ProfileCreateView
 
 app_name = 'profile'
 
 urlpatterns = [
-     path('',ProfileListView.as_view(),name='profile_list'),
+     path('',ProfileListView.as_view(),name='list'),
+     path('create/',ProfileCreateView.as_view(), name='create')
 ]
--- profile/views.py
+++ profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 from .models import Profile
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 
@@ -8,3 +10,10 @@ class ProfileListView(ListView):
     ListView.model  = Profile
     ListView.template_name = 'profile/profile_list.html'
     ListView.context_object_name = 'profile_list'
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    template_name = "profile/profile_create.html"
+    fields= '__all__'
+    success_url = reverse_lazy('profile:list')
+    
